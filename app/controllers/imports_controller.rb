d4a141d49c7e36afa2091355d33b07b611204c46
Isaac Priestley <progressions@gmail.com>
2014-04-13 15:10:18 -0500

    works so far

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -6,12 +6,22 @@ class ImportsController < ApplicationController
 
   def create
     uploaded_file = params[:import]
+
+    @revenue = 0
+    @items = []
+
     if uploaded_file.present?
       results = CSV.parse(uploaded_file.read, col_sep: "\t")
 
       headers = results.shift
+
       results.each do |row|
+        item = Item.from_tsv(row)
+        @items << item
+        @revenue += item.item_price.to_i
       end
     end
+
+    @revenue /= 100.0
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,19 @@
 #
 
 class Item < ActiveRecord::Base
+  def self.from_tsv(row)
+    item_price = row[2].to_f * 100
+
+    self.create(
+      purchaser_name: row[0],
+      item_description: row[1],
+      item_price: item_price,
+      purchase_count: row[3],
+      merchant_address: row[4],
+      merchant_name: row[5]
+    )
+  end
+
+  def self.gross_revenue_for(item_ids)
+  end
 end
