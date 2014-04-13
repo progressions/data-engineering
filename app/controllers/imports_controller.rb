ea753444f7883292ef7d0c706fea7b70c9aac3c4
Isaac Priestley <progressions@gmail.com>
2014-04-13 16:14:57 -0500

    use Import class in controller

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -8,23 +8,6 @@ class ImportsController < ApplicationController
   def create
     uploaded_file = params[:import]
 
-    @revenue = 0
-    @items = []
-
-    if uploaded_file.present?
-      results = CSV.parse(uploaded_file.read, col_sep: "\t")
-
-      headers = results.shift
-
-      results.each do |row|
-        purchase = Purchase.new(row)
-        @items << purchase
-        @revenue += purchase.gross_revenue
-      end
-    end
-
-    @revenue /= 100.0
-
     @import = Import.new(uploaded_file.read)
   end
 end
