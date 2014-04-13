614398455be41f06c5dbb348ab246407d3465bef
Isaac Priestley <progressions@gmail.com>
2014-04-13 16:01:46 -0500

    first spec

---
+++ app/models/purchase_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Purchase do
+  it "creates merchants, purchasers, items and line_items" do
+    content = File.read("#{Rails.root}/example_input.tab")
+    puts content
+  end
+end
