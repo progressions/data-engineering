b7aa1b4bcb70686808f429dec0eb72b96a3c88be
Isaac Priestley <progressions@gmail.com>
2014-04-13 15:45:58 -0500

    switch to using line item

--- lib/purchase.rb
+++ lib/purchase.rb
@@ -6,23 +6,23 @@
 class Purchase
   attr_accessor :row
 
-  delegate :gross_revenue, to: :item
-  delegate :valid?, to: :item
+  delegate :gross_revenue, to: :line_item
+  delegate :valid?, to: :line_item
 
   def initialize(row)
     self.row = row
 
-    item.save
+    line_item.save
   end
 
   def options
     unless @options
-      item_price = row[2].to_f * 100
+      price = row[2].to_f * 100
 
       @options = {
         purchaser_name: row[0],
-        item_description: row[1],
-        item_price: item_price,
+        description: row[1],
+        price: price,
         purchase_count: row[3],
         merchant_address: row[4],
         merchant_name: row[5]
@@ -31,14 +31,23 @@ class Purchase
     @options
   end
 
+  def line_item
+    unless @line_item
+      @line_item = LineItem.new(
+        purchaser: purchaser,
+        merchant: merchant,
+        item: item,
+        purchase_count: options[:purchase_count]
+      )
+    end
+    @line_item
+  end
+
   def item
     unless @item
       @item = Item.new(
-        purchaser: purchaser,
-        merchant: merchant,
-        item_description: options[:item_description],
-        item_price: options[:item_price],
-        purchase_count: options[:purchase_count]
+        description: options[:description],
+        price: options[:price]
       )
     end
     @item
@@ -46,7 +55,7 @@ class Purchase
 
   def purchaser
     unless @purchaser
-      Purchaser.where(name: options[:purchaser_name]) ||
+      @purchaser = Purchaser.where(name: options[:purchaser_name]) ||
         Purchaser.create(name: options[:purchaser_name])
     end
     @purchaser
@@ -54,7 +63,7 @@ class Purchase
 
   def merchant
     unless @merchant
-      Merchant.where(name: options[:merchant_name]) ||
+      @merchant = Merchant.where(name: options[:merchant_name]) ||
         Purchaser.create(name: options[:merchant_name], address: options[:merchant_address])
     end
     @merchant
